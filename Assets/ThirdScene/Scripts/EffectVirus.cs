8f66e469806372f0a2f6670d41011c0e54c4a830
wrg2674 <wrg267499@naver.com>
2024-07-23 15:10:47 +0900

    바이러스 움직이며 이동하기

---
+++ Assets/ThirdScene/Scripts/EffectVirus.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectVirus : MonoBehaviour
+{
+    [SerializeField] private GameObject deadEffect;
+    private float dtime = 0;
+    private int[] directions = new int[2];
+    private int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        directions[0] = -1;
+        directions[1] = 1;
+        direction = directions[Random.Range(0, 1)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dtime += Time.deltaTime;
+
+        if (dtime < 0.3f)
+        {
+            this.transform.Translate(new Vector3(3.0f*direction, 0.0f, 0.0f) * Time.deltaTime, Space.Self);
+        }
+        else 
+        {
+            dtime = 0.0f;
+            direction = -direction;
+        }
+    }
+}
