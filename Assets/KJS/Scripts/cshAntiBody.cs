64b85d82505d76dfb81ca859ea456d3d28f20a5f
BellLine99 <kimbell_line99@naver.com>
2024-07-24 13:26:35 +0900

    feat: 창 휘두르기
    창 휘두르면 바이러스에 항체 부착되어 약화됨

--- Assets/KJS/Scripts/cshAntiBody.cs
+++ Assets/KJS/Scripts/cshAntiBody.cs
@@ -5,29 +5,44 @@ using UnityEngine;
 public class cshAntiBody : MonoBehaviour
 {
 
+    private List<GameObject> antibodies;
+    public string SpeareTag;
+    public string AntibodyTag;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        antibodies = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            if (child.tag == AntibodyTag)
+            {
+                antibodies.Add(child.gameObject);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-            
+
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("attacked");
         if (collision.gameObject.tag == "Virus_A")
         {
-            Transform antibody = collision.gameObject.transform.Find("antibody");
-            if (antibody != null)
+            
+            List<GameObject> inactiveAntibodies = antibodies.FindAll(antibody => !antibody.activeSelf);
+            for (int i = 0; i < 2 && inactiveAntibodies.Count > 0; i++)
             {
-                antibody.gameObject.SetActive(true);
+                int randomIndex = Random.Range(0, inactiveAntibodies.Count);
+                inactiveAntibodies[randomIndex].SetActive(true);
+                inactiveAntibodies.RemoveAt(randomIndex);
             }
 
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
     }
 }
